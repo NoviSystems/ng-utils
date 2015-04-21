2ed102c0e212728e66b5dafb3676ac429867d6d3
Ryan P Kilby <rpkilby@ncsu.edu>
2015-04-21 19:58:22 -0400

    Added base setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='ng-utils',
+    version='0.1',
+    packages=['itng'],
+    include_package_data=True,
+    license='BSD License',
+    long_description=README,
+    url='https://github.com/itng/ng-utils/',
+    author='Ryan P Kilby',
+    author_email='rpkilby@ncsu.edu',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
