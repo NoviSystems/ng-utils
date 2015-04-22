14cf1efd5c4885b12599bdc54708bebfa5844190
Ryan P Kilby <rpkilby@ncsu.edu>
2015-04-21 22:06:01 -0400

    Added basic auth templates

--- itng/common/context_processors.py
+++ itng/common/context_processors.py
@@ -6,3 +6,9 @@ def debug(request):
     return {
         'debug': getattr(settings, 'DEBUG', False)
     }
+
+
+def auth_base(request):
+    return {
+        'auth_base': getattr(settings, 'REGISTRATION_AUTH_BASE', 'registration/auth/base.html')
+    }
