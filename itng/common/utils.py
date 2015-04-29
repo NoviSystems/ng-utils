919b485286d8399fd5eeb8933747c9b2a3904ab7
Ryan P Kilby <rpkilby@ncsu.edu>
2015-04-29 15:11:53 -0400

    Added request relative url reversing

---
+++ itng/common/utils.py
@@ -0,0 +1,22 @@
+
+from django.core import urlresolvers
+
+
+def relative_viewname(viewname, resolver):
+    """
+    """
+    return ':'.join(
+        filter(None, [
+            resolver.app_name, resolver.namespace, viewname
+        ])
+    )
+
+
+def reverse(viewname, request, urlconf=None, args=None, kwargs=None, current_app=None):
+    """
+    A wrapper around Django's builtin `django.core.urlresolvers.reverse` utility function
+    that will use the current request to derive the `app_name` and `namespace`. This is
+    most useful for apps that need to reverse their own URLs.
+    """
+    viewname = relative_viewname(viewname, request.resolver_match)
+    return urlresolvers.reverse(viewname, urlconf, args, kwargs, current_app)
