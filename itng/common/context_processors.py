9af2b8f31c2c90bd5a3ad9fc63b88d3c86d99d77
Ryan P Kilby <rpkilby@ncsu.edu>
2015-04-29 22:27:14 -0400

    Moved auth_base context processor.
    - auth_base context processor needed to live under the correct app.
    - auth_base context proceesor needed to be made optional.
    - templates default to extending "base.html" if no auth_base value

--- itng/common/context_processors.py
+++ itng/common/context_processors.py
@@ -6,9 +6,3 @@ def debug(request):
     return {
         'debug': getattr(settings, 'DEBUG', False)
     }
-
-
-def auth_base(request):
-    return {
-        'auth_base': getattr(settings, 'REGISTRATION_AUTH_BASE', 'registration/auth/base.html')
-    }
