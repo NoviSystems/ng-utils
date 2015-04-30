03157e86187996d51c4bea81cf724981492c4aad
Ryan P Kilby <rpkilby@ncsu.edu>
2015-04-29 21:03:49 -0400

    Better generic admin handling

--- itng/registration/backends/invite/admin.py
+++ itng/registration/backends/invite/admin.py
@@ -1,12 +1,23 @@
 
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
 from . import views
 
 
 class AdminInvitationView(views.InvitationView):
     template_name = 'admin/registration_invite.html'
+
+    def get_context_data(self, **kwargs):
+        form = self.get_form()
+
+        context = super(AdminInvitationView, self).get_context_data(**kwargs)
+        context['opts'] = get_user_model()._meta
+        context['adminform'] = admin.helpers.AdminForm(
+            form, [(None, {'fields': [field.name for field in form]})], {},
+        )
+        return context
 
 
 class UserInvitationAdmin(admin.ModelAdmin):
@@ -16,5 +27,9 @@ class UserInvitationAdmin(admin.ModelAdmin):
 
     def get_urls(self):
         return [
-            url(r'^invite/$', self.admin_site.admin_view(AdminInvitationView.as_view()), name='registration_invite'),
+            url(
+                r'^invite/$',
+                self.admin_site.admin_view(
+                    AdminInvitationView.as_view(form_class=self.get_invite_form())),
+                name='registration_invite'),
         ] + super(UserInvitationAdmin, self).get_urls()
