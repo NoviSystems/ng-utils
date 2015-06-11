5a10cd66afd3ab76232123d684905c9ccb4bf879
Ryan P Kilby <rpkilby@ncsu.edu>
2015-06-11 17:43:52 -0400

    Added input field components

---
+++ itng/common/static/fields/block.js
@@ -0,0 +1,10 @@
+
+import BaseField from 'fields/base';
+import template from 'fields/templates/block.html!text'
+
+
+var InlineField = BaseField.extend({
+    template: template,
+});
+
+export default InlineField;
