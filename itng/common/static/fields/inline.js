e11a933cd865eaa7ffc693391ad90c36127f79b8
Ryan P Kilby <rpkilby@ncsu.edu>
2015-06-24 13:02:34 -0400

    style/bug fix for inline form

--- itng/common/static/fields/inline.js
+++ itng/common/static/fields/inline.js
@@ -1,10 +1,10 @@
 
 import BaseField from 'fields/base';
-import template from 'fields/templates/inline.html!text'
+import template from 'fields/templates/inline.html!text';
 
 
 var InlineField = BaseField.extend({
-    paramAttributes: ['label', 'help-text', 'required', 'errors', 'label-class', 'input-class', ],
+    props: ['label', 'help-text', 'required', 'errors', 'label-class', 'input-class', ],
     template: template,
 
     data: function () {
@@ -13,7 +13,7 @@ var InlineField = BaseField.extend({
             hasError: false,
             labelClass: 'col-md-3 col-sm-5',
             inputClass: 'col-md-9 col-sm-7',
-        }
+        };
     }
 });
 
