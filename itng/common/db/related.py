2a6cda550d16035581ece1c2d18f8f31e23b4ecc
Ryan P Kilby <rpkilby@ncsu.edu>
2016-02-24 16:54:32 -0500

    Update ManyThroughManyField to work w/ django 1.9

--- itng/common/db/related.py
+++ itng/common/db/related.py
@@ -1,5 +1,4 @@
 
-import django
 from django.db.models.fields import related
 from django.utils.functional import cached_property
 from ..utils import override_attr
@@ -35,53 +34,31 @@ def create_many_related_manager(superclass, rel):
 # For reference:
 # - https://code.djangoproject.com/ticket/9475
 # - https://groups.google.com/forum/#!topic/django-developers/uWe31AjzZX0
-class ManyRelatedObjectsDescriptor(related.ManyRelatedObjectsDescriptor):
+class ManyToManyDescriptor(related.ManyToManyDescriptor):
 
     @cached_property
     def related_manager_cls(self):
         return create_many_related_manager(
-            super(ManyRelatedObjectsDescriptor, self).related_manager_cls,
-            self.related.field.rel,
+            super(ManyToManyDescriptor, self).related_manager_cls,
+            self.rel,
         )
 
     def __set__(self, instance, value):
-        opts = self.related.field.rel.through._meta
+        opts = self.through._meta
 
         with override_attr(opts, 'auto_created', True):
-            super(ManyRelatedObjectsDescriptor, self).__set__(instance, value)
-
-
-class ReverseManyRelatedObjectsDescriptor(related.ReverseManyRelatedObjectsDescriptor):
-
-    @cached_property
-    def related_manager_cls(self):
-        return create_many_related_manager(
-            super(ReverseManyRelatedObjectsDescriptor, self).related_manager_cls,
-            self.field.rel,
-        )
-
-    def __set__(self, instance, value):
-        opts = self.field.rel.through._meta
-
-        with override_attr(opts, 'auto_created', True):
-            super(ReverseManyRelatedObjectsDescriptor, self).__set__(instance, value)
+            super(ManyToManyDescriptor, self).__set__(instance, value)
 
 
 class ManyThroughManyField(related.ManyToManyField):
 
-    def __init__(self, *args, **kwargs):
-        if django.VERSION >= (1, 9):
-            raise Exception("Nooop. Ensure compatibility w/ Django 1.9 before proceeding.")
-
-        super(ManyThroughManyField, self).__init__(*args, **kwargs)
-
     def contribute_to_class(self, cls, name, **kwargs):
         super(ManyThroughManyField, self).contribute_to_class(cls, name, **kwargs)
 
-        setattr(cls, self.name, ReverseManyRelatedObjectsDescriptor(self))
+        setattr(cls, self.name, ManyToManyDescriptor(self.remote_field, reverse=False))
 
     def contribute_to_related_class(self, cls, related):
         super(ManyThroughManyField, self).contribute_to_related_class(cls, related)
 
         if not self.rel.is_hidden() and not related.related_model._meta.swapped:
-            setattr(cls, related.get_accessor_name(), ManyRelatedObjectsDescriptor(related))
+            setattr(cls, related.get_accessor_name(), ManyToManyDescriptor(self.remote_field, reverse=True))
