aaab0f368ae359ef632412717f6f1ca85af6e807
Spencer Hitchins <jshitchi@ncsu.edu>
2016-04-19 15:58:52 -0400

    Upload size validation for base64 image field

--- itng/common/rest/fields.py
+++ itng/common/rest/fields.py
@@ -1,6 +1,8 @@
 import base64, re, uuid
 
+from django.conf import settings
 from django.core.files.base import ContentFile
+from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers
 
 
@@ -10,4 +12,10 @@ class Base64ImageField(serializers.ImageField):
         data_type, data = data.split(',', 1)
         ext = re.split('[:;//]', data_type)[2]
         data = ContentFile(base64.b64decode(data), name='{}.{}'.format(str(uuid.uuid4()), ext))
-        return super(Base64ImageField, self).to_internal_value(data)
+        data = super(Base64ImageField, self).to_internal_value(data)
+        self.validate_upload_size(data)
+        return data
+
+    def validate_upload_size(self, value):
+        if value.size > settings.MAX_UPLOAD_SIZE:
+            raise serializers.ValidationError('The file you uploaded is larger than 2MB, the maximum file upload size')
